19d76c0a1f9353be23895e6f9c59e78941f5e8d4
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-04-15 17:46:21 +0100

    android

--- Android/Nodes/app/src/main/java/com/example/nodes/MainActivity.java
+++ Android/Nodes/app/src/main/java/com/example/nodes/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.nodes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),"Some one pressed Simba's button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
