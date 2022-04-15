b0ff4ee35470a35c0f32ebf8cdc55191abf001f4
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-04-15 14:05:26 +0100

    ip change

--- p2p-me/msg.py
+++ p2p-me/msg.py
@@ -1,7 +1,7 @@
 import socket
 import time
 
-UDP_IP = "10.18.112.66"
+UDP_IP = "10.18.113.159"
 UDP_Port = 5005
 tunnel = (UDP_IP, UDP_Port)
 sock = socket.socket(socket.AF_INET, socket.DGRAM )# interet,udp
