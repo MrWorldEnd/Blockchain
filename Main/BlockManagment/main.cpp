207dc1f4ec2b67fcf04db9acb66e0f163179ce13
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2022-12-25 23:43:23 +0000

    Bug fixes and applications compiled

--- Main/BlockManagment/main.cpp
+++ Main/BlockManagment/main.cpp
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <fstream>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <cstring>
+
 #include "Socket.h"
 
 #include "Blockchain.h"
@@ -13,104 +18,4 @@
 
 using namespace std;
 
-PacketPayload genPacketPayload(Device user,Device item){
-    PacketPayload x = PacketPayload(user,item);
-    return x;
-}
-
-
-vector<Packet>buffer;
-
 int main(){}
-
-void packetreader(Packet x)
-{
-  if (x.getPinfo() == "blockchaindata")
-    {
-      listenforblockdata(packet);
-      validate(buffer);
-      ifblock(buffer);
-      updatebuffer();
-    }
-  else if (x.getPinfo() == "tx packet")
-    {
-      listenfortx();
-      validate(tx);
-      actuate(tx);
-      broadcastpackt(tx);
-    }
-}
-int transactionport = 8080;
-int dttport = 8081;
-int dta = 8082;
-        
-void listenfortxdata(){
-  try {
-      // Normally you'd spawn threads for multiple connections.
-         Connection conn = PortListener(transactionport).waitForConnection();
-         conn.tx("Transaction message receved");
-         cout << "Hello message sent" << endl;
-
-         Packet input = conn.rx();
-
-        } catch (runtime_error &e) {
-                cerr << e.what() << endl;
-                return EXIT_FAILURE;
-        }
-};
-
-void listenforDTT(){
-  try {
-    // Normally you'd spawn threads for multiple connections.
-    Connection conn = PortListener(8081).waitForConnection();
-    conn.tx("DTT message receved");
-    cout << "Hello message sent" << endl;
-
-    Packet input = conn.rx();
-
-    } catch (runtime_error &e) {
-    cerr << e.what() << endl;
-    return EXIT_FAILURE;
-  }
-}
-
-void listenforDTA(){
-  try {
-    // Normally you'd spawn threads for multiple connections.
-    Connection conn = PortListener(8082).waitForConnection();
-    conn.tx("DTA message receved");
-    cout << "Hello message sent" << endl;
-
-    Packet input = conn.rx();
-
-    } catch (runtime_error &e) {
-    cerr << e.what() << endl;
-    return EXIT_FAILURE;
-  }
-}
-
-void SendPacket(Packet x)
-{
-try {
-  Connection conn("127.0.0.1",8080);
-  conn.tx("message ");
-  string s = conn.rx();
-  cout << s << endl;
-
-  Packet inputpacket;
-
-  } catch (exception &e) {
-    cerr << e.what() << endl;
-    EXIT_FAILURE;
-  }
-}
-
-      input buffer;
-      thread1:
-        broadcastNodeinfo();
-        listenfornodeinfo();
-      thread2:
-        listenfortxpacket();
-        listenforblockchaindata();
-
-};
