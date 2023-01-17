c17306c2f96eb8af63b890972f49aef599f00d51
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2023-01-17 04:06:45 +0000

    intitializer

---
+++ Main/BlockManagment/Intitializer.cpp
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sqlite3.h>
+#include <cstring>
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
+   int i;
+   for(i = 0; i<argc; i++) {
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+   }
+   printf("\n");
+   return 0;
+}
+
+static int callback(void* data, int argc, char** argv, char** azColName)
+{
+	int i;
+	fprintf(stderr, "%s: ", (const char*)data);
+
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	printf("\n");
+	return 0;
+}
+
+static int callback(void* data, int argc, char** argv, char** azColName)
+{
+	int i;
+	fprintf(stderr, "%s: ", (const char*)data);
+
+	for (i = 0; i < argc; i++) {
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+
+	printf("\n");
+	return 0;
+}
+
+int main(int argc, char* argv[]) {
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char const *sql;
+   
+   /* Open database */
+   rc = sqlite3_open("test.db", &db);
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return(0);
+   } else {
+      fprintf(stdout, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "CREATE TABLE PeerList(" \
+      "ID INT PRIMARY KEY NOT NULL," \
+      "NAME TEXT NOT NULL," \
+      "ADDRESS CHAR(50)";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ) {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Table created successfully\n");
+   }
+   sqlite3_close(db);
+   return 0;
+}
+
+#include "device.cpp"
+#include "key.h"
+
+int main ()
+{
+    //Initialize
+
+};
+
+void initializepeertable(){
+   //conect to peer list from sql database
+
+
+   sqlite3 *db;
+   char *zErrMsg = 0;
+   int rc;
+   char const *sql;
+   
+   /* Open database */
+   rc = sqlite3_open("peerlist.db", &db);
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+      return;
+   } else {
+      fprintf(stdout, "Opened database successfully\n");
+   }
+
+   /* Create SQL statement */
+   sql = "CREATE TABLE PeerList(" \
+      "ID INT PRIMARY KEY NOT NULL," \
+      "NAME TEXT NOT NULL," \
+      "ADDRESS CHAR(50)";
+
+   /* Execute SQL statement */
+   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   if( rc != SQLITE_OK ) {
+      fprintf(stderr, "SQL error: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+   } else {
+      fprintf(stdout, "Table created successfully\n");
+   }
+   sqlite3_close(db);
+    
+   //load peer list
+
+   //load check for current block chain state
+
+   //continuouse loop for blockchain update
+};
+
+void loadPeerlist(){
+	sqlite3* DB;
+	int exit = 0;
+	exit = sqlite3_open("test.db", &DB);
+	string data("CALLBACK FUNCTION");
+
+	string sql("SELECT * FROM PeerList;");
+	if (exit) {
+		std::cerr << "Error open DB " << sqlite3_errmsg(DB) << std::endl;
+		return;
+	}
+	else
+		std::cout << "Opened Database Successfully!" << std::endl;
+
+	int rc = sqlite3_exec(DB, sql.c_str(), callback, (void*)data.c_str(), NULL);
+
+	if (rc != SQLITE_OK)
+		cerr << "Error SELECT" << endl;
+	else {
+		cout << "Operation OK!" << endl;
+	}
+
+	sqlite3_close(DB);
+};
