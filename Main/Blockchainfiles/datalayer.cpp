c6c6a5d13937ec31b4e165084050c622d83f9019
TeveredziSJ <58471235+MrWorldEnd@users.noreply.github.com>
2023-08-15 10:43:41 +0100

    fixed SQL woring on rapidjson

--- Main/Blockchainfiles/datalayer.cpp
+++ Main/Blockchainfiles/datalayer.cpp
@@ -1,6 +1,8 @@
 #include "blockchain.cpp"
 #include <sqlite3.h>
 
+using namespace rapidjson;
+using namespace std;
 //sql
 void createTable() {
     sqlite3* db;
@@ -10,26 +12,17 @@ void createTable() {
     rc = sqlite3_open("example.db", &db);
     if (rc) {
         std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+        return;
+    } else { 
+        std::cout << "Opened database successfully!" << std::endl;
+        return;
     }
 
     // SQL statement to create a table
-    const char* sql = "CREATE TABLE IF NOT EXISTS Peers ("
-                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                      "devicename TEXT NOT NULL,"
-                      "blockchainstate INTEGER NOT NULL,"
-                      "ip TEXT NOT NULL"
-                      
-                      "CREATE TABLE IF NOT EXISTS Blockchain ("
-                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                      "currentHash TEXT NOT NULL,"
-                      "previousHash INTEGER NOT NULL,"
-                      
-                      "CREATE TABLE IF NOT EXISTS devices ("
-                      "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                      "name TEXT,"
-                      "securitylevel INTEGER NOT NULL,"
-                      "serverstatus bool"
-                      ");";
+    const char* sql = "CREATE TABLE BLOCKCHAIN (" \
+                      "ID INT PRIMARY KEY AUTOINCREMENT," \
+                      "CURREMTHASH TEXT," \
+                      "PREVIOUSHASH INT);" ;
 
     // Execute the SQL statement
     rc = sqlite3_exec(db, sql, 0, 0, 0);
@@ -41,6 +34,8 @@ void createTable() {
 
     // Close the database connection
     sqlite3_close(db);
+    std:cout << "Database closed\n";
+    return;
 };
 
 void addblock2db(Block x) {
@@ -52,46 +47,33 @@ void addblock2db(Block x) {
     if (rc) {
         std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
         return;
-    } else {
-        std::cout << "Opened database successfully!" << std::endl;
-    }
-
-    // Create a table if it doesn't exist
-    
-    const char* createTableSQL = "CREATE TABLE IF NOT EXISTS MyVariables (ID INTEGER PRIMARY KEY, Value TEXT);";
-    rc = sqlite3_exec(db, createTableSQL, 0, 0, 0);
-
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << sqlite3_errmsg(db) << std::endl;
-        sqlite3_close(db);
+    } else { 
+        std::cout << "Opened database successfully!\n" << std::endl;
         return;
-    } else {
-        std::cout << "Table created successfully!" << std::endl;
     }
 
-    // Example variable
-    int myVariable = 42;
-
-    // Insert the variable into the table
+// Insert the variable into the table
     
-    std::string insertSQL = "INSERT INTO Blockchain (id,data,currentHash,previousHash) VALUES ('" +to_string(x.getIndex())  +"','" + x.getData().getstr() +"','"+ x.getHash() +"','"+ x.getPrevHash() +"');";
+    std::string insertSQL = "INSERT INTO BLOCKCHAIN (id,data,currentHash,previousHash) VALUES ('" +to_string(x.getIndex())  +"','" + x.getData().getstr() +"','"+ x.getHash() +"','"+ x.getPrevHash() +"');";
     rc = sqlite3_exec(db, insertSQL.c_str(), 0, 0, 0);
 
     if (rc != SQLITE_OK) {
         std::cerr << "SQL error: " << sqlite3_errmsg(db) << std::endl;
+        return;
     } else {
-        std::cout << "Variable inserted successfully!" << std::endl;
+        std::cout << "Variable inserted successfully!\n" << std::endl;
+        return;
     }
 
     // Close the database
     sqlite3_close(db);
 }
 
-//json
 
 
 Value jBlock (Block x)
 {
+    cout<< "json sub program";
     Document d;
     d.SetObject();
 
@@ -121,6 +103,7 @@ Value jBlock (Block x)
 
 void addblocktofile(Block x, string filename)
 {
+    cout<<"Adding block to Json started\n";
     Document d;
     d.SetObject();
 
@@ -128,24 +111,19 @@ void addblocktofile(Block x, string filename)
 
     size_t sz = allocator.Size();
 
-    //something
-    Value blocktoadd;
-    blocktoadd = jBlock(x);
-
-    d.AddMember("Block", blocktoadd, allocator);
+    d.AddMember("Block", jBlock(x), allocator);
 
     // Convert JSON document to string
     StringBuffer strbuf;
     PrettyWriter<StringBuffer> writer(strbuf);
     d.Accept(writer);
-
     string temp=strbuf.GetString();
 
     // Create and open a text file
     ofstream myFile(filename);
-
     myFile << temp;
 
     myFile.close();
+    return;
 }
 
--- Main/Blockchainfiles/main.cpp
+++ Main/Blockchainfiles/main.cpp
@@ -4,23 +4,31 @@ std::string filename =  ("blockchain.json");
 std::string requestbuffer =("buffer.json");
 
 Blockchain initializeNode() {
+    std::cout << "initializeNode Started\n";
     Blockchain x;
     createTable();
-    ofstream MyFile(filename);
-    ofstream MyBuffer(requestbuffer);
+    ofstream myFile(filename);
+    ofstream myBuffer(requestbuffer);
     x.isvalid();
     x.printChain();
-    MyFile.close();
-    MyBuffer.close();
+    myFile.close();
+    myBuffer.close();
+    std::cout << "initializeNode completed\n";
     return x;
 }
 
 Blockchain test(){
+    std:cout << "test program start \n";
     Blockchain x;
-    Block tx;
-    x.addBlock(tx);
-    addblocktofile(tx,filename);
-    addblock2db(tx);
+    Block block;
+
+    std::cout << "Add to chain \n";
+    x.addBlock(block);
+    std::cout << "Add to JSON \n";
+    addblocktofile(block,filename);
+    std::cout << "Add to DB \n";
+    addblock2db(block);
+    std::cout << "test program copmlete \n";
     return x;
 }
 
